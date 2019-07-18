920506f087f347cd46be21a298f5e448cd9c2433
Aaron Haughton <AaronJHaughton@outlook.com>
2019-07-17 22:32:53 -0400

    add two labels with name and hobbies

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hobbiesLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLabels()
     }
 
 
+    func setupLabels() {
+        nameLabel.text = "Aaron Haughton"
+        hobbiesLabel.text = "Biking and video games"
+    }
+    
 }
 
