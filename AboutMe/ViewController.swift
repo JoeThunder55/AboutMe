7b1a6b68529dbdfb7d861072ddcb20e4076c068a
Aaron Haughton <AaronJHaughton@outlook.com>
2019-07-17 21:56:22 -0400

    Initial Commit; Line 15 - prints my name.

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hi. My name is(huh?) My Name is (wha?) My name is (Who?) Slim Shady.")
     }
 
 
